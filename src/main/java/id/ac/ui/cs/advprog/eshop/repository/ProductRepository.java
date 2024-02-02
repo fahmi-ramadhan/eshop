73110362df3ff2a35348453adb084fec1f8aaf45
fahmi-ramadhan <fahmi122004@gmail.com>
2024-02-02 17:08:11 +0700

    feat: add `findById` and `update` method

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -19,4 +19,19 @@ public class ProductRepository {
     public Iterator<Product> findAll() {
         return productData.iterator();
     }
+
+    public Product findById(String productId) {
+        for (Product product : productData) {
+            if (product.getProductId().equals(productId)) {
+                return product;
+            }
+        }
+        return null;
+    }
+
+    public Product update(Product originalProduct, Product editedProduct) {
+        originalProduct.setProductName(editedProduct.getProductName());
+        originalProduct.setProductQuantity(editedProduct.getProductQuantity());
+        return originalProduct;
+    }
 }
