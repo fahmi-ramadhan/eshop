28b035f8fc68b9f2953f3370613cf3b7a3eff025
fahmi-ramadhan <fahmi122004@gmail.com>
2024-02-02 18:01:31 +0700

    feat: add "delete" product controller

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -48,4 +48,11 @@ public class ProductController {
         service.update(editedProduct);
         return "redirect:../list";
     }
+
+    @PostMapping("/delete/{productId}")
+    public String deleteProduct(@PathVariable String productId) {
+        Product product = service.findById(productId);
+        service.delete(product);
+        return "redirect:../list";
+    }
 }
