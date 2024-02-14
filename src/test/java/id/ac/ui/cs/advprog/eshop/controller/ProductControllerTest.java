ca812d574336cf51db246870bd7709f2bc45e050
Fahmi Ramadhan <51454564+fahmi-ramadhan@users.noreply.github.com>
2024-02-14 20:20:06 +0700

    refactor: change string style from camelCase to PascalCase

--- src/test/java/id/ac/ui/cs/advprog/eshop/controller/ProductControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/ProductControllerTest.java
@@ -34,7 +34,7 @@ class ProductControllerTest {
     @Test
     void testCreateProductPage() {
         String viewName = productController.createProductPage(model);
-        assertEquals("createProduct", viewName);
+        assertEquals("CreateProduct", viewName);
         verify(model).addAttribute(eq("product"), any(Product.class));
     }
 
@@ -52,7 +52,7 @@ class ProductControllerTest {
         when(productService.findAll()).thenReturn(productList);
 
         String viewName = productController.productListPage(model);
-        assertEquals("productList", viewName);
+        assertEquals("ProductList", viewName);
         verify(model).addAttribute("products", productList);
     }
 
@@ -63,7 +63,7 @@ class ProductControllerTest {
         when(productService.findById(productId)).thenReturn(product);
 
         String viewName = productController.editProductPage(productId, model);
-        assertEquals("editProduct", viewName);
+        assertEquals("EditProduct", viewName);
         verify(model).addAttribute("product", product);
     }
 
