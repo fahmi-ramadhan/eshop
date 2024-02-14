bedb47228568f56c611deb7d448a1ccef50a42bd
Fahmi Ramadhan <51454564+fahmi-ramadhan@users.noreply.github.com>
2024-02-14 11:16:49 +0700

    fix: add assertion to EshopApplicationTests

--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
@@ -2,13 +2,19 @@ package id.ac.ui.cs.advprog.eshop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import id.ac.ui.cs.advprog.eshop.controller.ProductController;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class EshopApplicationTests {
 
+	@Autowired
+	private ProductController controller;
 	@Test
 	void contextLoads() {
 		EshopApplication.main(new String[] {});
+		assertThat(controller).isNotNull();
 	}
 
 }
